788156a85394c28366c6531c75dcdfd28c23689f
Driviz <kumar.driviz@gmail.com>
2020-11-10 18:41:49 +0530

    First Commit

---
+++ src/main/java/tech/driviz/Covid19CountryDataExtractor/Covid19CountryDataExtractorApplication.java
@@ -0,0 +1,13 @@
+package tech.driviz.Covid19CountryDataExtractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19CountryDataExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19CountryDataExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/driviz/Covid19CountryDataExtractor/Covid19CountryDataExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.driviz.Covid19CountryDataExtractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19CountryDataExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
