f5ff41bc18c9bd870b627e179c7891fc61c8372c
Driviz <kumar.driviz@gmail.com>
2020-11-10 20:27:33 +0530

    adapted listener

--- src/main/java/tech/driviz/Covid19CountryDataExtractor/Listeners/ScheduledListener.java
+++ src/main/java/tech/driviz/Covid19CountryDataExtractor/Listeners/ScheduledListener.java
@@ -1,11 +1,15 @@
 package tech.driviz.Covid19CountryDataExtractor.Listeners;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import tech.driviz.Covid19CountryDataExtractor.Services.CovidDataExtractorService;
+import tech.driviz.Covid19CountryDataExtractor.Services.CovidDataSavingService;
+import tech.driviz.Covid19CountryDataExtractor.models.CountryCasesData;
 
 @Component
 public class ScheduledListener {
@@ -13,14 +17,19 @@ public class ScheduledListener {
 	public static final Logger logger = LoggerFactory.getLogger(ScheduledListener.class);
 
 	private final CovidDataExtractorService covidDataExtractorService;
+	private final CovidDataSavingService covidDataSavingService;
 
-	public ScheduledListener(CovidDataExtractorService covidDataExtractorService) {
+	public ScheduledListener(CovidDataExtractorService covidDataExtractorService,
+			CovidDataSavingService covidDataSavingService) {
 		this.covidDataExtractorService = covidDataExtractorService;
+		this.covidDataSavingService = covidDataSavingService;
 	}
 
-	@Scheduled(fixedRate = 10000)
+	@Scheduled(fixedRate = 100000)
 	public void listen() {
 		logger.info("intitiating extracting Data");
-		covidDataExtractorService.extractData();
+		List<CountryCasesData> dataList = covidDataExtractorService.extractData();
+		covidDataSavingService.save(dataList);
+		logger.info("data saved to db");
 	}
 }
